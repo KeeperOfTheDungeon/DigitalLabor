7c9f78df9d8a211889c67bf2f24ec48cf6a37431
Oktavian Gniot <tavin@tavin.de>
2023-03-10 17:56:32 +0100

    Initial upload
    Signed-off-by: Oktavian Gniot <tavin@tavin.de>

---
+++ Source/Aufgabe2_1.s
@@ -0,0 +1,17 @@
+/*
+ * Aufgabe_2_1.S
+ *
+ *  Created on: 10.03.2023
+ *      Author: tavin
+ *
+ *	Aufgabe : Addition
+ */
+.text /* Specify that code goes in text segment */
+.code 32 /* Select ARM instruction set */
+.global _start /* Specify global symbol */
+_start:
+
+stop:
+	nop
+	bal stop
+.end
---
+++ Source/BeispielAufgabe1_4.s
@@ -0,0 +1,18 @@
+/*
+ * BeispielAufgabe_1.S
+ *
+ *  Created on: 02.10.2020
+ *      Author: tavin
+ *
+ *	Aufgabe : Addition
+ */
+.text /* Specify that code goes in text segment */
+.code 32 /* Select ARM instruction set */
+.global _start /* Specify global symbol */
+_start:
+
+
+stop:
+	nop
+	bal stop
+.end
